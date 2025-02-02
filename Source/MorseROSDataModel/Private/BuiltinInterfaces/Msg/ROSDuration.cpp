3e7ec30238ec7610bf69234e7326c5b9f592bf83
Nicolas Brugie <nicolasbrugie@gmail.com>
2025-02-02 16:40:57 +0100

    Move function implementation in cpp

--- Source/MorseROSDataModel/Private/BuiltinInterfaces/Msg/ROSDuration.cpp
+++ Source/MorseROSDataModel/Private/BuiltinInterfaces/Msg/ROSDuration.cpp
@@ -1,1 +1,13 @@
 #include "BuiltinInterfaces/Msg/ROSDuration.h"
+
+void FROSDuration::DDSToUE(const builtin_interfaces_msg_Duration& InData)
+{
+	Sec = InData.sec;
+	Nanosec = InData.nanosec;
+};
+
+void FROSDuration::UEToDDS(builtin_interfaces_msg_Duration& OutData) 
+{
+	OutData.sec = Sec;
+	OutData.nanosec = Nanosec;
+};
